938f95824ec86a85917a25db2b31b6acc07cf4bb
DJ_Kovrik <djkovrik@gmail.com>
2017-06-02 11:37:59 +0300

    Add base application class

---
+++ app/src/main/java/com/sedsoftware/bakingapp/BakingApp.java
@@ -0,0 +1,11 @@
+package com.sedsoftware.bakingapp;
+
+import android.app.Application;
+
+public class BakingApp extends Application {
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+  }
+}
