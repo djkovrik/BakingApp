e6f0ce7e47976ba1611383ee54219655bc04f0d1
DJ_Kovrik <djkovrik@gmail.com>
2017-06-02 11:47:57 +0300

    Add main launcher activity

---
+++ app/src/main/java/com/sedsoftware/bakingapp/features/recipelist/RecipeListActivity.java
@@ -0,0 +1,14 @@
+package com.sedsoftware.bakingapp.features.recipelist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.sedsoftware.bakingapp.R;
+
+public class RecipeListActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_recipe_list);
+  }
+}
