3fff7783ab888a41682edfa35857fdb4ed30d227
DJ_Kovrik <djkovrik@gmail.com>
2017-06-02 12:06:48 +0300

    Init libraries in application class

--- app/src/main/java/com/sedsoftware/bakingapp/BakingApp.java
+++ app/src/main/java/com/sedsoftware/bakingapp/BakingApp.java
@@ -1,11 +1,29 @@
 package com.sedsoftware.bakingapp;
 
 import android.app.Application;
+import com.facebook.stetho.Stetho;
+import com.squareup.leakcanary.LeakCanary;
+import timber.log.Timber;
 
 public class BakingApp extends Application {
 
   @Override
   public void onCreate() {
     super.onCreate();
+
+    if (LeakCanary.isInAnalyzerProcess(this)) {
+      // This process is dedicated to LeakCanary for heap analysis.
+      // You should not init your app in this process.
+      return;
+    }
+    LeakCanary.install(this);
+    // Normal app init code below.
+
+    if (BuildConfig.DEBUG) {
+      Timber.uprootAll();
+      Timber.plant(new Timber.DebugTree());
+
+      Stetho.initializeWithDefaults(this);
+    }
   }
 }
