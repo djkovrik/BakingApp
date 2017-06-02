a1dc3c434bd69e5a099cfb6b6317aeb9b75e74d7
DJ_Kovrik <djkovrik@gmail.com>
2017-06-02 19:32:32 +0300

    Add activity utils

---
+++ app/src/main/java/com/sedsoftware/bakingapp/utils/ActivityUtils.java
@@ -0,0 +1,16 @@
+package com.sedsoftware.bakingapp.utils;
+
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+
+public class ActivityUtils {
+
+  public static void addFragmentToActivity(@NonNull FragmentManager fragmentManager,
+      @NonNull Fragment fragment, int frameId) {
+    FragmentTransaction transaction = fragmentManager.beginTransaction();
+    transaction.add(frameId, fragment);
+    transaction.commit();
+  }
+}
