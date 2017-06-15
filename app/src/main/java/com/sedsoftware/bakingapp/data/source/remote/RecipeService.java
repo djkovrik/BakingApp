928803c03db2a66015405b28e0bb3f8338650f9e
DJ_Kovrik <djkovrik@gmail.com>
2017-06-15 20:55:18 +0300

    Use direct endpoint

--- app/src/main/java/com/sedsoftware/bakingapp/data/source/remote/RecipeService.java
+++ app/src/main/java/com/sedsoftware/bakingapp/data/source/remote/RecipeService.java
@@ -7,9 +7,9 @@ import retrofit2.http.GET;
 
 public interface RecipeService {
 
-  String ENDPOINT = "https://go.udacity.com";
+  String ENDPOINT = "https://d17h27t6h515a5.cloudfront.net";
 
-  @GET("/android-baking-app-json")
+  @GET("/topher/2017/May/59121517_baking/baking.json")
   Observable<List<Recipe>> loadRecipesFromServer();
 
 }
